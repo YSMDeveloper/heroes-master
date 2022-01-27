a1479c9ffd07bc275d73f25458ab074dafd97b4c
YSMDeveloper <ysmdeveloper@gmail.com>
2022-01-27 16:12:45 -0300

    Pantalla de error añadida

---
+++ error.php
@@ -0,0 +1,21 @@
+<head>
+    <link href="css\error.css" rel="stylesheet" type="text/css">
+
+</head>
+<?php include("template/cabecera.php"); ?>
+
+<div class="error__container">
+    <div class="error__mensaje">
+        <div class="error__mensaje-header">
+            UPS...
+        </div>
+        <div class="error__mensaje-body">
+            Creo que la tormenta lo ha llevado a un sitio equivocado.
+        </div>
+        <div class="error__mensaje-pie">
+            Será redireccionado inmediatamente
+        </div>
+    </div>
+</div>
+
+<?php include("template/pie.php"); ?>
